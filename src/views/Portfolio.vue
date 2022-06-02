7a01d8b9691c113244336c169373f4d3026bd3d3
AndyBeable <andy.beable@gmail.com>
2022-06-02 15:50:31 +0100

    portfolio restructure

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,7 +1,30 @@
 <template>
   <div class="portfolio">
-    <!--    <h1>Portfolio</h1>-->
-
+    <div class="portfolio__tile">
+      <div class="portfolio__title">
+        <h2>Magazines</h2>
+      </div>
+    </div>
+    <div class="portfolio__tile">
+      <div class="portfolio__title">
+        <h2>Podcasts</h2>
+      </div>
+    </div>
+    <div class="portfolio__tile">
+      <div class="portfolio__title">
+        <h2>Videos</h2>
+      </div>
+    </div>
+    <div class="portfolio__tile">
+      <div class="portfolio__title">
+        <h2>Creative Direction</h2>
+      </div>
+    </div>
+    <div class="portfolio__tile">
+      <div class="portfolio__title">
+        <h2>Books</h2>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,8 +39,25 @@ export default {
 
 <style lang="scss" scoped>
 .portfolio {
-  padding: 7rem 0;
+  padding: 8rem 0;
   width: 80%;
   margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  @media screen and (min-width: 750px) {
+    flex-direction: row;
+  }
+
+  &__tile {
+    padding: 5rem;
+    background-color: #fff;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  }
+
+  &__title {
+    text-transform: uppercase;
+  }
 }
 </style>
